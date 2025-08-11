aa552bdd34c83ca0f442de5eb46ff8484b0999db
Sanveer Singh <sanveersingh.ss4@gmail.com>
2025-08-11 17:27:02 +0530

    Patterns

--- Patterns/plus_star.cpp
+++ Patterns/plus_star.cpp
@@ -3,11 +3,5 @@ using namespace std;
 int main() {
     int row;
     cin>>row;
-    int middle = 2*(row-1)/2;
-    for (int i=0; i<row;i++) {
-        for (int j=middle+1; j<=i;j++) {
-            cout<<"*";
-        }
-        cout<<endl;
-    }
+
 }
